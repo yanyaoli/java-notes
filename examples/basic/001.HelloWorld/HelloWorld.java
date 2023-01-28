a249ebda0027a2f7a7e37644be3dad3d51338fa2
isxyz <cuit@email.com>
2023-01-28 23:49:08 +0800

    Update

---
+++ examples/basic/001.HelloWorld/HelloWorld.java
@@ -0,0 +1,7 @@
+// 创建文件 HelloWorld.java(文件名需与类名一致)
+public class HelloWorld {
+    public static void main(String[] args) {    
+        // 注：String args[] 与 String[] args 都可以执行，但推荐使用 String[] args，这样可以避免歧义和误读。
+        System.out.println("Hello World");
+    }
+}
---
+++ examples/字符串/字符串比较/StringCompareEmp.java
@@ -0,0 +1,10 @@
+// 创建字符串
+public class StringCompareEmp {
+    public static void main(String[] args) {
+        String str = "Hello World";    
+        String anotherString = "hello World";
+        Object objStr = str;
+
+        System.out.println(str.compareTo(anotherString));
+    }
+}
