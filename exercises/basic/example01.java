d90be1b9f2d123f14be52ea4ac0239d254e81429
isxyz <cuit@email.com>
2023-02-20 00:40:40 +0800

    Update

--- exercises/basic/example01.java
+++ exercises/basic/example01.java
@@ -1,18 +1,18 @@
-package exercises.basic;
-
-/*
+/**
  * 象棋口诀
  * 先使用 char 型变量定义 “马” “象” “卒” 这3个棋子
  * 再输出 “马走日，象走田，小卒一去不复还” 的象棋口诀。
- * */
+ */
+
+package exercises.basic;
 
 public class example01 {
     public static void main(String[] args) {
 
-        char ma = '马';
-        char xiang = '象';
-        char zu = '卒';
+        char horse = '马';
+        char elephant = '象';
+        char soldier = '卒';
 
-        System.out.println(ma + "走日，" + xiang + "走田，小" + zu + "一去不复还");
+        System.out.println(horse + "走日，" + elephant + "走田，小" + soldier + "一去不复还");
     }
 }
---
+++ exercises/basic/example03.java
@@ -0,0 +1,36 @@
+/**
+ * 输出个人信息
+ * 控制台输出如下所示的个人基本信息。
+ * 
+ * 个人基本信息
+ * ------------------------
+ * 姓名：  李四
+ * 性别：  男
+ * 年龄：  25
+ * 身高：  1.76米
+ * 体重：  65.5千克
+ * 是否已婚：  false
+ */
+
+package exercises.basic;
+
+public class example03 {
+    public static void main(String[] args) {
+        
+        String name = "李四";
+        String sex = "男";
+        String age = "25";
+        double height = 1.75;
+        double weight = 65.5;
+        boolean married = false;
+
+        System.out.println("个人基本信息\n");
+        System.out.println("------------------");
+        System.out.println("姓名： " + name);
+        System.out.println("性别： " + sex);
+        System.out.println("年龄： " + age);
+        System.out.println("身高： " + height + "米");
+        System.out.println("体重： " + weight + "千克");
+        System.out.println("是否已婚：  " + married);
+    }
+}
