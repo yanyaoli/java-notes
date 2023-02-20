1477b59974867e8e0c0e0d1828241acfbf2dac98
isxyz <cuit@email.com>
2023-02-20 13:53:13 +0800

    Update

---
+++ exercises/basic/example05.java
@@ -0,0 +1,13 @@
+/**
+ * 计算商和余数
+ * 应用除法运算符可以计算两个数的商，应用取余运算符可以计算两个数相除所得的余数。
+ * 根据这两个运算符做一个数字转置的练习，将123的各数字顺序前后颠倒后输出。
+ */
+
+package exercises.basic;
+
+public class example05 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ exercises/basic/example06.java
@@ -0,0 +1,20 @@
+/**
+ * 判断成绩会否及格
+ * 当分数大于等于60时，成绩及格，否则不及格。
+ * 现一学生的分数是80分，使用三元运算符判断这名学生的成绩能否及格。
+ */
+
+package exercises.basic;
+
+public class example06 {
+    public static void main(String[] args) {
+        double score = 80;
+
+        if(score >= 60){
+            System.out.println("成绩及格");
+        }else{
+            System.out.println("成绩不及格");
+        }
+
+    }
+}
